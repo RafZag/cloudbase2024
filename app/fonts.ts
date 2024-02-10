218e066add7bb931de0d3c49ccb670f0f7b2764e
Rafal <zagorny.rafal@gmail.com>
2024-02-10 15:57:09 +0100

    initial commit

---
+++ app/fonts.ts
@@ -0,0 +1,4 @@
+import { Space_Grotesk, Teko } from 'next/font/google';
+
+export const space_grotesk = Space_Grotesk({ weight: ['400', '600'], subsets: ['latin'] });
+export const teko = Teko({ weight: ['500'], subsets: ['latin'] });
